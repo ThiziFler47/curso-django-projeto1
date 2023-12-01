df536eaf1e67f40634852edf2eea7cc76ba2367d
Francisco Yago Freire Dos Santos <thizilplays8606@gmail.com>
2023-12-01 16:37:59 -0300

    retirando sobre e contato

--- projeto/urls.py
+++ projeto/urls.py
@@ -1,7 +1,5 @@
 from django.contrib import admin
 from django.urls import path, include
-from recipes.views import home, contato, sobre
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- recipes/views.py
+++ recipes/views.py
@@ -1,22 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
-# from httpx import get
-# from httpx import AsyncClient
-# import json
-# Create your views here.
 
 
 def home(request):
     # async with AsyncClient() as client:
     #     blaze_response = await client.get("https://blaze-4.com/api/crash_games/history?")
     #     return JsonResponse(json.loads(blaze_response.text))
-    return render(request=request, template_name="global/home.html")
+    return render(request=request, template_name="recipes/home.html", context={"name": "Yago Freire"})
 
-
-def contato(request):
-    return HttpResponse("CONTATO")
-
-
-def sobre(request):
-
-    return HttpResponse("SOBRE")
